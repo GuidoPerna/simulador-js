7c56fe2cef91c6e0322b3dd92bdd0e1fb0146936
Guido Perna <guido.perna@gmail.com>
2022-09-06 12:24:00 -0300

    agregando hof

--- index.js
+++ index.js
@@ -20,15 +20,15 @@ let productoDos = new Producto("Producto Dos", 250, 20);
 let productoTres = new Producto("Producto Tres", 300, 15);
 let total = 0;
 
-let Inventario = [{productoUno}, {productoDos}, {productoTres}];
+let Inventario = [productoUno, productoDos, productoTres];
+// console.log(Inventario[0].precio);
 
-for(const item of Inventario){
-    console.log(item);
-};
+// for(const item of Inventario){
+//     console.log(item);
+// };
 
-
-// let Precios = [100, 250, 300];
-// let StockProductos = [10, 20, 15];
+let pocoStock = Inventario.filter((item) => item.cantidad < 14);
+console.table(pocoStock);
 
 let pedido = alert("Seleccione al menos un producto");
 
@@ -36,31 +36,31 @@ let carrito = prompt("Seleccione entre 1, 2 o 3");
 
 switch(carrito){
     case "1":
-        alert(suma(total, total, Inventario.precio[0]));
+        alert(suma(total, total, Inventario[0].precio));
         break;
 
     case "2":
-        alert(suma(total, total, Precios[1]));
+        alert(suma(total, total, Inventario[1].precio));
         break;
     
     case "3":
-        alert(suma(total, total, Precios[2]));
+        alert(suma(total, total, Inventario[2].precio));
         break;
 
     case"1 2":
-        alert(suma(total, Inventario.precio[0], Inventario.precio[1]));
+        alert(suma(total, Inventario[0].precio, Inventario[1].precio));
         break;
 
     case"1 3":
-        alert(suma(total, Precios[0], Precios[2]));
+        alert(suma(total, Inventario[0].precio, Inventario[2].precio));
         break;
 
     case"2 3":
-        alert(suma(total, Precios[1], Precios[2]));
+        alert(suma(total, Inventario[1].precio, Inventario[2].precio));
         break;
 
     case"1 2 3":
-        alert(suma(Precios[0], Precios[1], Precios[2]));
+        alert(suma(Inventario[0].precio, Inventario[1].precio, Inventario[2].precio));
         break;
 
     default:
