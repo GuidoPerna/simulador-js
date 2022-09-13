414a9e9590c942d8d599b8960e7a35e47f2d00c3
Guido Perna <guido.perna@gmail.com>
2022-09-12 23:34:01 -0300

    se agrega eventos

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ class Producto {
     }
 
     vender() {
+        this.vendido = true;
         this.cantidad = this.cantidad - 1;
     }
 }
@@ -21,21 +22,58 @@ let productoTres = new Producto("Producto Tres", 300, 15);
 let total = 0;
 
 let Inventario = [productoUno, productoDos, productoTres];
-// console.log(Inventario[0].precio);
 
 // for(const item of Inventario){
 //     console.log(item);
 // };
 
-let pocoStock = Inventario.filter((item) => item.cantidad < 14);
-console.table(pocoStock);
+// let pocoStock = Inventario.filter((item) => item.cantidad < 14);
+// console.table(pocoStock);
 
 // Inventario.sort((a, b) => a.cantidad - b.cantidad);
 // console.table(Inventario)
 
 let pedido = alert("Seleccione al menos un producto");
 
-function carrito(){
+let Carrito = [];
+
+function restarProdUno(){
+    productoUno.vender();
+    Carrito.push(productoUno);
+    alert("Agregaste el Producto Uno al carrito");
+    console.log(Carrito);
+}
+let boton1 = document.getElementById("boton1");
+let evento = "click";
+boton1.addEventListener("click", restarProdUno);
+
+function restarProdDos(){
+    productoDos.vender();
+    Carrito.push(productoDos);
+    alert("Agregaste el Producto Dos al carrito");
+    console.log(Carrito);
+}
+let boton2 = document.getElementById("boton2");
+boton2.addEventListener("click", restarProdDos);
+
+function restarProdTres(){
+    productoTres.vender();
+    Carrito.push(productoTres);
+    alert("Agregaste el Producto Tres al carrito");
+    console.log(Carrito);
+}
+let boton3 = document.getElementById("boton3");
+boton3.addEventListener("click", restarProdTres);
+
+function reinicio(){
+    Carrito.splice(0, 50);
+    alert("Carrito eliminado");
+    console.log(Carrito);
+}
+let botonReinicio = document.getElementById("botonReinicio");
+botonReinicio.addEventListener("click", reinicio);
+
+function opciones(){
     
 let compra = prompt("Seleccione entre 1, 2 o 3");
 
@@ -74,7 +112,7 @@ switch (compra) {
 }
 }
 
-carrito();
+opciones();
 
 let respuesta = prompt("Completaste tu pedido?");
 
@@ -86,7 +124,7 @@ if (respuesta.toLowerCase() != "si") {
 
 while (respuesta.toLowerCase() != "si") {
     let pedido = alert("Seleccione al menos un producto");
-    carrito ();
+    opciones ();
 
     respuesta = prompt("Completaste tu pedido?");
 
