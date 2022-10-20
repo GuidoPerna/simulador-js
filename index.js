a50bc58f02bf3d59c412210ab9f454ff6897d57c
Sebastián <sebastian.morsaline@gmail.com>
2022-10-19 23:07:13 -0300

    Simplificacion de codigo + funcionalidad

--- index.js
+++ index.js
@@ -22,12 +22,15 @@ let carrito = [];
  * Evento que se dispara cuando ya se cargó toda la pagina
  */
 window.addEventListener('load', async (event) => {
+    await loadAll()
+});
+
+const loadAll = async () => {
     readStorage();
     inventario = await getInventory();
     renderAllInventario();
     renderAllCarrito();
-});
-
+}
 
 const getInventory = () => {
     return new Promise( (resolve, reject) => {
@@ -42,20 +45,36 @@ const getInventory = () => {
 /**
  * Si el producto ya existe en el carrito devuelve su index. Caso contrario, devuelve -1
  */
-const findProductoEnCarrito = (idProducto) => {
+ const findProductoEnCarrito = (idProducto) => {
     return carrito.findIndex( item => item.id === idProducto );
 }
 
-const agregarAlCarrito = async (idProducto) => {
+/**
+ * Actualiza el inventario, aumentando ó disminuyendo la cantidad de un producto segun el valor recibido
+ */
+const handleInventarioCantidad = (id, cantidad = 0) => {
+    const index = inventario.findIndex( i => i.id === id );
+    if (index > -1) {
+        inventario[index].cantidad = inventario[index].cantidad + cantidad;
+        renderAllInventario();
+    }
+}
+
+/**
+ * Actualiza el carrito, aumentando ó disminuyendo la cantidad de un producto segun el valor recibido
+ */
+const handleCarrito = async (idProducto, cantidad = 1) => {
     const itemInventario = inventario.find(item => item.id === idProducto); // Busco en el inventario
     const busquedaEnCarrito = findProductoEnCarrito(idProducto); // Busco en carrito
 
     if (  busquedaEnCarrito > -1 ) {
-        ++carrito[busquedaEnCarrito].cantidad; // Ya existe, le sumo uno
+        carrito[busquedaEnCarrito].cantidad = carrito[busquedaEnCarrito].cantidad + cantidad;
     } else {
-        const nuevo = { ...itemInventario, cantidad: 1 };
+        const nuevo = { ...itemInventario, cantidad };
         carrito.push( nuevo ); // Agrego uno nuevo
     }
+
+    handleInventarioCantidad(itemInventario.id, -1 * cantidad); // Actualizo el INVENTARIO
     renderAllCarrito(); // Redibuja todo el carrito en pantalla
 
     saveToStorage(); // Guardar todo el carrito en el storage
@@ -63,29 +82,7 @@ const agregarAlCarrito = async (idProducto) => {
     Swal.fire({
         icon: 'success',
         position: 'top',
-        title: `Se agregó un "${itemInventario.nombre}"`,
-        toast: true,
-        showConfirmButton: false,
-        timer: 1500,
-    });
-}
-
-const quitarDelCarrito = async (idProducto) => {
-    const itemInventario = inventario.find(item => item.id === idProducto); // Busco en el inventario
-    const busquedaEnCarrito = findProductoEnCarrito(idProducto); // Busco en carrito
-
-    if (busquedaEnCarrito > -1) {
-        --carrito[busquedaEnCarrito].cantidad; // Ya existe, le resta uno
-    }
-
-    renderAllCarrito(); // Redibuja todo el carrito en pantalla
-
-    saveToStorage(); // Guardar todo el carrito en el storage
-
-    Swal.fire({
-        icon: 'success',
-        position: 'top',
-        title: `Se agregó un "${itemInventario.nombre}"`,
+        title: `Se ${cantidad > 0 ? 'agregó' : 'eliminó'} ${Math.abs(cantidad)} "${itemInventario.nombre}"`,
         toast: true,
         showConfirmButton: false,
         timer: 1500,
@@ -95,19 +92,17 @@ const quitarDelCarrito = async (idProducto) => {
 const renderAllInventario = async () => {
     const containerInventario = document.getElementById("containerInventario");
     containerInventario.innerHTML = ''; // Limpio la pantalla
-    inventario.forEach(element => {
+    inventario.forEach(el => {
         const producto = document.createElement("li");
         producto.innerHTML = `
-            <span> ${element.cantidad} </span>
-            <i> "${element.nombre?.toUpperCase()}" </i>
-            <strong> $${element.precio} </strong>
-            <button class="btn btn-success btn-sm ms-3" id="agregar${element.nombre}">Agregar</button>
+            <span> ${el.cantidad} </span>
+            <i> "${el.nombre?.toUpperCase()}" </i>
+            <strong> $${el.precio} </strong>
+            <button class="btn btn-success btn-sm ms-3" onclick="handleCarrito(${el.id}, 1)">➕</button>
+            <button class="btn btn-outline-success btn-sm" onclick="handleCarrito(${el.id}, ${el.cantidad})">Full</button>
         `;
         producto.classList.add('mt-1');
         containerInventario.append(producto);
-
-        const boton = document.getElementById(`agregar${element.nombre}`);
-        boton.addEventListener("click", () => agregarAlCarrito(element.id) )
     });
 }
 
@@ -122,19 +117,17 @@ const renderAllCarrito = () => {
             <span> "${el.nombre}" </span>
             <strong> $${el.precio}</strong>
             <strong> ░ Total: $ ${el.precio * el.cantidad} ░ </strong>
-            <button id="eliminar${el.id}" class="btn btn-danger btn-sm" >Quitar</button>
+            <button class="btn btn-warning btn-sm" onclick="handleCarrito(${el.id}, -1)">➖</button>
+            <button class="btn btn-outline-danger btn-sm" onclick="handleCarrito(${el.id}, ${-1 * el.cantidad})">Vaciar</button>
         `;
         contenedorCarrito.appendChild(item);
-
-        const boton = document.getElementById(`eliminar${el.id}`);
-        boton.addEventListener("click", () => quitarDelCarrito(el.id))
     });
 }
 
-const limpiarCarrito = () => {
+const limpiarCarrito = async () => {
     carrito = [];
-    renderAllCarrito();
     clearStorage();
+    loadAll();
 }
 
 const botonReinicio = document.getElementById("botonReinicio");
@@ -149,12 +142,12 @@ botonReinicio.addEventListener("click", () =>
         confirmButtonText: 'Borrar'
     }).then( (result) => {
         if (result.isConfirmed) {
+            limpiarCarrito();
             Swal.fire(
                 'El carrito ha sido vaciado!',
                 'Puedes iniciar tu pedido nuevamente.',
                 'success'
             )
-            limpiarCarrito();
         }
     })
 );
